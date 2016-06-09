270dba75ef3efefed16c33421c3e7ca5954b8e31
Zenon Zeniou <zzeniou86@gmail.com>
2016-06-09 15:29:30 +0100

    more work on decorators
    Signed-off-by: Zenon Zeniou <zzeniou86@gmail.com>

--- decorators.ts
+++ decorators.ts
@@ -1,35 +1,59 @@
 interface IComponentDef {
-    name?: string;
-    module?: string;
+    /**
+     * Element selector for component in camelCase.
+     */
+    selector: string;
+    components?: string[];
 }
 
-function copyObject(target: Object, source: Object): Object {
-    for (let property in source) {
-        if (source.hasOwnProperty(property)) {
-            target[property] = source[property];
-        }
-    }
-    return {};
+function dashToUppercase(str: string) {
+    return str.replace(/-([a-z])/g, function (m, w) { return w.toUpperCase(); });
 }
+
 
 export function Component(config: ng.IComponentOptions & IComponentDef): ClassDecorator {
-    return function (target: any): ng.IComponentOptions {
-        config.controller = target;
-        config.templateUrl = "views/" + config.templateUrl;
-        if (!config.hasOwnProperty("bindings")) {
-            config.bindings = {};
-        }
-        copyObject(config.bindings, target.prototype.zz$$bindings);
-        delete target.prototype.zz$$input;
-        return config;
+    return function (target: any): any {
+
+        // set component options
+        let componentOptions: ng.IComponentOptions = {};
+        componentOptions.controller = target;
+        componentOptions.controllerAs = config.controllerAs;
+        componentOptions.templateUrl = "views/" + config.templateUrl;
+        componentOptions.template = config.template;
+        componentOptions.bindings = Object.assign({}, config.bindings, target.prototype.zz$$bindings);
+        componentOptions.require = config.require;
+        componentOptions.transclude = config.transclude;
+        delete target.prototype.zz$$bindings;
+
+        target.$inject = ["$q"];
+
+        angular.module(target.name, [])
+            .component(dashToUppercase(config.selector), componentOptions);
+        return target;
     };
 }
 
-export function Input(config?: any): PropertyDecorator {
+export function Input(symbol?: any): PropertyDecorator {
     return function (target: any, propertyKey: string | symbol): void {
         if (!target.zz$$bindings) {
             target.zz$$bindings = {};
         }
-        target.zz$$bindings[propertyKey] = "<";
+        symbol = ( symbol !== undefined && (symbol === "<" || symbol === "@") ) ? symbol : "<";
+        target.zz$$bindings[propertyKey] = symbol;
+    };
+}
+
+export function Output(): PropertyDecorator {
+    return function (target: any, propertyKey: string | symbol): void {
+        if (!target.zz$$bindings) {
+            target.zz$$bindings = {};
+        }
+        target.zz$$bindings[propertyKey] = "&";
+    };
+}
+
+export function Inject(arr: string[]): ClassDecorator {
+    return function (target: any): any {
+        target.$inject = arr;
     };
 }
